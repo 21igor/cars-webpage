53f4a2eb5dfa99d959365fe2e698c41eb5c6f404
Shatha Shaath and Igor Seleznov <igor@>
2016-06-01 13:50:13 -0700

    add car-webpage form for name and address

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("form#name").submit(function(event) {
+    event.preventDefault();
+    var firstName = $("input#first-name").val();
+    var lastName = $("input#last-name").val();
+    var address = $("input#address").val();
+    var postalcode = $("input#postal-code").val();
+    var city = $("input#city").val();
+
+    $(".first-name").text(firstName);
+    $(".last-name").text(lastName);
+    $(".address").text(address);
+    $(".postalcode").append(postalcode);
+    $(".city").text(city);
+    
+    $("#receipt").show();
+
+    event.preventDefault();
+  });
+});
